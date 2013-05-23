1b8368136ce3a07d2995ce1873b21af44e2f4eb0
Markus Muth <e1127793@student.tuwien.ac.at>
2013-05-24 00:36:06 +0200

    Date Tests und DateDao started ...

---
+++ src/main/java/at/ac/tuwien/sepm/dao/hsqldb/ExceptionMessages.java
@@ -0,0 +1,10 @@
+package at.ac.tuwien.sepm.dao.hsqldb;
+
+/**
+ * Author: MUTH Markus
+ * Date: 5/24/13
+ * Time: 1:37 AM
+ * Description of class "ExceptionMessages":
+ */
+public class ExceptionMessages {
+}
