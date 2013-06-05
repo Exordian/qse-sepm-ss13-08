023068543fc6aa08bf431b032e765c222b6930b8
Plaz Georg <e1126779@student.tuwien.ac.at>
2013-06-05 04:11:14 +0200

    fixed broken tests

---
+++ src/test/java/at/ac/tuwien/sepm/service/impl/LVAServiceImplTest.java
@@ -0,0 +1,68 @@
+package at.ac.tuwien.sepm.service.impl;
+
+import at.ac.tuwien.sepm.service.ServiceException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Author: Georg Plaz
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/applicationContext-test.xml"})
+public class LVAServiceImplTest {
+    @Autowired
+    LVAServiceImpl lvaService;
+    @Before
+    public void setUp() throws Exception {
+        //lvaService = new LVAServiceImpl();
+    }
+
+    @Test(expected = ServiceException.class)
+    public void testCreateNull() throws Exception {
+        lvaService.create(null);
+    }
+
+    /*@Test(expected = ServiceException.class)
+    public void testReadByIdNull() throws Exception {
+        lvaService.readById(null);
+    }
+
+    @Test(expected = ServiceException.class)
+    public void testReadByMetaLvaNull() throws Exception {
+        lvaService.readByMetaLva(null);
+    }*/
+
+    @Test(expected = ServiceException.class)
+    public void testReadUncompletedByYearSemesterStudyProgressNull() throws Exception {
+        lvaService.readUncompletedByYearSemesterStudyProgress(0,null,false);
+    }
+
+    /*@Test(expected = ServiceException.class)
+    public void testReadByIdWithoutLvaDatesNull() throws Exception {
+        lvaService.readByIdWithoutLvaDates()
+    }
+
+    @Test(expected = ServiceException.class)
+    public void testReadByYearAndSemesterNull() throws Exception {
+        lvaService.readByYearAndSemester(0,false);
+    }*/
+
+    @Test(expected = ServiceException.class)
+    public void testUpdateNull() throws Exception {
+        lvaService.update(null);
+    }
+
+    @Test(expected = ServiceException.class)
+    public void testValidateLVANull() throws Exception {
+        lvaService.validateLVA(null);
+    }
+
+    /*@Test(expected = ServiceException.class)
+    public void testValidateIDNull() throws Exception {
+
+    }*/
+}
