4e72ef49177c9a4e2a82e7ca321558532a397b71
Jakob Englisch <e1125164@student.tuwien.ac.at>
2013-06-10 18:54:33 +0200

    fixed package mess

--- src/main/java/at/ac/tuwien/sepm/ui/studyProgress/display/SemesterPanel.java
+++ src/main/java/at/ac/tuwien/sepm/ui/studyProgress/display/SemesterPanel.java
@@ -2,6 +2,7 @@ package at.ac.tuwien.sepm.ui.studyProgress.display;
 
 import at.ac.tuwien.sepm.entity.MetaLVA;
 import at.ac.tuwien.sepm.ui.StandardInsidePanel;
+import at.ac.tuwien.sepm.ui.metaLva.MetaLVADisplayPanel;
 
 import javax.swing.*;
 import java.util.ArrayList;
